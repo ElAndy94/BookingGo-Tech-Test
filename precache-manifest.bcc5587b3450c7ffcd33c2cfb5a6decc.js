8756be4e62fc26a15893459e82df3d8894338da4
Andrew <andrewpeliza@hotmail.com>
2019-10-14 14:09:34 +0100

    Updates

---
+++ precache-manifest.bcc5587b3450c7ffcd33c2cfb5a6decc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa1992607b16fd0b04ee767ff282166",
+    "url": "/BookingGo-Tech-Test/index.html"
+  },
+  {
+    "revision": "eaa5ea1e3a926634baaa",
+    "url": "/BookingGo-Tech-Test/static/css/main.1b080101.chunk.css"
+  },
+  {
+    "revision": "66952cf5110c138ac72b",
+    "url": "/BookingGo-Tech-Test/static/js/2.f3aa29c3.chunk.js"
+  },
+  {
+    "revision": "eaa5ea1e3a926634baaa",
+    "url": "/BookingGo-Tech-Test/static/js/main.c44f2675.chunk.js"
+  },
+  {
+    "revision": "fbedad110d6a4c827614",
+    "url": "/BookingGo-Tech-Test/static/js/runtime-main.3353d468.js"
+  }
+]);
